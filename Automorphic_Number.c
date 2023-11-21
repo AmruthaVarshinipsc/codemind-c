d55673907458d58003d477cc275d8b9cfe6f65c9
P SC AMRUTHA VARSHINI <120082740+AmruthaVarshinipsc@users.noreply.github.com>
2023-11-21 14:57:50 +0530

    #include <stdio.h>
    int checkAutomorphic(int num){
        
        int square = num * num;
        
        while(num != 0)
        {
            
            if(num % 10 != square % 10){
                return 0;
            }
            
            
            num /= 10;
            square /= 10;
        }
        
        return 1;
    }
    
    int main ()
    {
        int num ;
        scanf("%d",&num);
        int square = num * num ;
        
        if(checkAutomorphic(num))
            printf("Automorphic Number");
        else
            printf("Not an Automorphic Number");
        
    
    }

---
+++ Automorphic_Number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int checkAutomorphic(int num){
+    
+    int square = num * num;
+    
+    while(num != 0)
+    {
+        
+        if(num % 10 != square % 10){
+            return 0;
+        }
+        
+        
+        num /= 10;
+        square /= 10;
+    }
+    
+    return 1;
+}
+
+int main ()
+{
+    int num ;
+    scanf("%d",&num);
+    int square = num * num ;
+    
+    if(checkAutomorphic(num))
+        printf("Automorphic Number");
+    else
+        printf("Not an Automorphic Number");
+    
+
+}
